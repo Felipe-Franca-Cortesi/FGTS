fbdd6bfae692d83da09f9e6806e333978e9b53ed
Felipe França <contatofelipefranca12@gmail.com>
2020-09-28 11:28:35 -0300

    Resolução do exercício FGTS

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double porcentagemEntrada = 8;
+
+            Console.Clear();
+            Console.WriteLine("----FGTS");
+            Console.Write("Informe o valor do salário (R$)..: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+            
+            double contaPorcentagem = porcentagemEntrada / 100 * salario;
+            Console.WriteLine($"FGTS (R$): {contaPorcentagem}");
+
+        }
+    }
+}
